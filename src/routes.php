f2b0929eb29b50c2f5901773ef3f37b548d2c52b
Sandeep Bangarh <35298606+sanbangarh309@users.noreply.github.com>
2018-08-28 19:03:48 +0530

    route file

--- src/routes.php
+++ src/routes.php
@@ -3,8 +3,12 @@ Route::group(['namespace' => 'sandeep\BangarhSocial\Controllers'], function()
 {
 	Route::get('/', 'ButtonController@index');
     Route::match(['get', 'post'], '/api/sauth', 'FbController@sanAuthorization');
-    Route::match(['get', 'post'], '/api/lauth', 'LineController@sanAuthorization');
+    Route::match(['get', 'post'], '/api/line', 'LineController@sanAuthorization');
+    // Route::match(['get', 'post'], '/api/telegram', function(){
+    // 	return App::call('TelegramController@sanSendMessage');
+    // });
+    Route::match(['get', 'post'], '/api/telegram', 'TelegramController@sanSendMessage');
 	// Route::get('/admin', 'AdminController@index');
-})
+});
 
 ?>
