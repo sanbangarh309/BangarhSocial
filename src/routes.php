dc23a0c7d07f75e4429a2781a694d38b99f5bcae
Sandeep Bangarh <35298606+sanbangarh309@users.noreply.github.com>
2018-08-30 11:04:10 +0530

    route updated

--- src/routes.php
+++ src/routes.php
@@ -1,13 +1,15 @@
 <?php
 Route::group(['namespace' => 'sandeep\BangarhSocial\Controllers'], function()
 {
+	Route::get('/fb', 'ButtonController@facebook');
 	Route::get('/', 'ButtonController@index');
     Route::match(['get', 'post'], '/api/sauth', 'FbController@sanAuthorization');
     Route::match(['get', 'post'], '/api/line', 'LineController@sanAuthorization');
     // Route::match(['get', 'post'], '/api/telegram', function(){
     // 	return App::call('TelegramController@sanSendMessage');
     // });
-    Route::match(['get', 'post'], '/api/telegram', 'TelegramController@sanSendMessage');
+    Route::match(['get', 'post'], '/api/telegram', 'TelegramController@getMe');
+    Route::match(['get', 'post'], '/api/slack', 'SlackController@sanSendMessage');
 	// Route::get('/admin', 'AdminController@index');
 });
 
