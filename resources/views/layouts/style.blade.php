a730d88ce31bdaa855e964f74690e77908179e39
Sandeep Bangarh <35298606+sanbangarh309@users.noreply.github.com>
2018-09-05 10:44:43 +0530

    style file added

---
+++ resources/views/layouts/style.blade.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link type="text/css" rel="stylesheet" href="{{ San_Helpers::san_asset('/css/style.css') }}">
+<link type="text/css" rel="stylesheet" href="{{ San_Helpers::san_asset('/css/bootstrap.min.css') }}">
+<link type="text/css" rel="stylesheet" href="{{ San_Helpers::san_asset('/css/custom.css') }}">
